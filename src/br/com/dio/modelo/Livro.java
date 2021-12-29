0f5cefcd3a2d9a1a01b08008ea360b5e0d5d6767
Sandro <Sandro@Destrov>
2021-12-28 21:46:51 -0300

    Primeiro commit utilizando a IDE Eclipse

---
+++ src/br/com/dio/modelo/Livro.java
@@ -0,0 +1,65 @@
+package br.com.dio.modelo;
+
+public class Livro {
+
+	private String titulo;
+	private String autor;
+
+	public Livro(String titulo, String autor) {
+		this.titulo = titulo;
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (autor == null) {
+			if (other.autor != null)
+				return false;
+		} else if (!autor.equals(other.autor))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro: " + titulo + ", " + autor;
+	}
+
+}
---
+++ src/br/com/dio/teste/ProgramaPrincipal.java
@@ -0,0 +1,14 @@
+package br.com.dio.teste;
+
+import br.com.dio.modelo.Livro;
+
+public class ProgramaPrincipal {
+	public static void main(String[] args) {
+		Livro livro01 = new Livro("A Travessia", "William P. Young");
+		Livro livro02 = new Livro("A Cabana", "William P. Young");
+		
+		System.out.println(livro01);
+		System.out.println(livro02);
+	}
+
+}
