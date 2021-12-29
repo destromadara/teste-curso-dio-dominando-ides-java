85ce5bde348dbece81709477de2b21ec4be8d069
destromadara <destrov@hotmail.com>
2021-12-29 00:12:30 -0300

    Atualizando TesteLivro e incluindo calculadora simples

--- src/br/com/dio/teste/TesteLivro.java
+++ src/br/com/dio/teste/TesteLivro.java
@@ -2,7 +2,7 @@ package br.com.dio.teste;
 
 import br.com.dio.modelo.Livro;
 
-public class Teste {
+public class TesteLivro {
 	public static void main(String[] args) {
 		Livro livro01 = new Livro("A Travessia", "William P. Young");
 		Livro livro02 = new Livro("A Cabana", "William P. Young");
